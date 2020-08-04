82739980b77335d83b39c16c8f26301934a25bbf
Gleb <gleb.savkin.kiev@gmail.com>
2020-08-04 23:43:21 +0300

    testing api

--- bot.py
+++ bot.py
@@ -1,16 +1,35 @@
 import telebot
 import os
 from flask import Flask, request
+import requests
 
 TOKEN = "1364491220:AAE_T1pkCAiaaeq-fnNkzx1GyIzcfsCzgFQ"
 
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
+nameSearchQuery = '''
+query ($id: Int) {
+  Media (search: "tanya", type: ANIME) {
+    title {
+      romaji
+      english
+    }
+    siteUrl
+    description
+  }
+}
+'''
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, 'Welcome!')
+
+
+@bot.message_handler(commands=['whatDisUwu'])
+def find_anime(message):
+    bot.reply_to(message, message.text)
 
 
 @server.route('/' + TOKEN, methods=['POST'])
