6b27277b0c0867cd33dbcf36d2cdfa36bec89906
Gleb <gleb.savkin.kiev@gmail.com>
2020-08-28 19:03:34 +0300

    working search and alias functions

---
+++ anilist_api_logic.py
@@ -0,0 +1,51 @@
+import requests
+import aliases_manager
+
+url = 'https://graphql.anilist.co'
+
+anime_name_search_query = '''
+query ($search: String) {
+  Media (search: $search, type: ANIME) {
+    title {
+      romaji
+      english
+    }
+    siteUrl
+    idMal
+    description
+    isAdult
+  }
+}
+'''
+
+anime_name_search_reply_pattern = '''
+<b>Назва:</b> {}
+<b>Посилання:</b> <a href="{}">Anilist</a>|<a href="{}">MAL</a>
+'''
+
+
+def find_anime_by_name_on_anilist(anime_name):
+    if aliases_manager.alias_exists(anime_name):
+        anime_name = aliases_manager.get_alias(anime_name)
+
+    response = requests.post(url, json={'query': anime_name_search_query, 'variables': {'search': anime_name}})
+
+    if response.status_code == 200:
+
+        json_response = (response.json())["data"]["Media"]
+
+        if json_response['isAdult']:
+            return "По моїй інформації цей тайтл має обмеження 18+. За правилами цього чату заборонено " \
+                   "надсилати контент порнографічного характеру"
+
+        if json_response['title']['english'] is None:
+            display_name = json_response['title']['romaji']
+        else:
+            display_name = json_response['title']['english']
+
+        return anime_name_search_reply_pattern.format(
+                         display_name,
+                         json_response['siteUrl'], "https://myanimelist.net/anime/{}".format(json_response["idMal"])
+                     )
+    else:
+        return "Хм, схоже такого аніме не існує, або не вдається знайти нічого схожого. Спробуй інший запит. "
--- bot.py
+++ bot.py
@@ -1,37 +1,100 @@
+import re
 import telebot
-import os
-from flask import Flask, request
-import requests
+import anilist_api_logic
+import aliases_manager
+from flask import Flask
 
 TOKEN = "1364491220:AAE_T1pkCAiaaeq-fnNkzx1GyIzcfsCzgFQ"
 
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
-nameSearchQuery = '''
-query ($id: Int) {
-  Media (search: "tanya", type: ANIME) {
-    title {
-      romaji
-      english
-    }
-    siteUrl
-    description
-  }
-}
-'''
+empty_anime_search_request = "Ця команда призначена для швидкого пошуку аніме. Натисни на кнопку" \
+                      "\"відповісти\" на повідомленні з назвою щоб ініціювати пошук. Альтернативно, " \
+                      "ти можеш виділити назву аніме в своєму повідомленні фігурними дужками, і пошук " \
+                      "відбудеться автоматично, наприклад так: {Jojo}"
+
+empty_alias_add_request = "Ця команда дозволяє додати сталу відповідність \"назва\"-\"аніме\". Це корисно якщо " \
+                          "пошук за якоїсь причини не може знайти необхідний тайтл. Наприклад: \n\n" \
+                          "/add_alias аріфурета == Arifureta: From Commonplace to World's Strongest \n\n" \
+                          "Зверни увагу на пробіли біля дорівнює!"
+
+empty_alias_delete_request = "Ця команда дозволяэ видалити alias. Наприклад: \n\n" \
+                             "/delete_alias аріфурета"
 
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, 'Welcome!')
-
-
-@bot.message_handler(commands=['whatDisUwu'])
+@bot.message_handler(commands=['what_dis_uwu'])
 def find_anime(message):
-    bot.reply_to(message, message.text)
+
+    if message.reply_to_message is None:
+        anime_name = message.text.replace('/what_dis_uwu', '')
+        if anime_name == '':
+            bot.reply_to(message, empty_anime_search_request)
+            return
+    else:
+        reply_text = message.reply_to_message.text
+        name_found = re.search("{.+}", reply_text)
+        if name_found is None:
+            anime_name = reply_text
+        else:
+            anime_name = name_found.group()[1:-1]
+    reply_message = anilist_api_logic.find_anime_by_name_on_anilist(anime_name.strip())
+    bot.reply_to(message, reply_message, parse_mode="HTML")
 
 
+@bot.message_handler(regexp="{.+}")
+def find_anime_in_brackets(message):
+    search_result = re.search("{.+}", message.text)
+    anime_name = search_result.group()[1:-1]
+    reply_message = anilist_api_logic.find_anime_by_name_on_anilist(anime_name.strip())
+    bot.reply_to(message, reply_message, parse_mode="HTML")
+
+
+@bot.message_handler(commands=['add_alias'])
+def add_alias(message):
+    message_text = message.text.replace('/add_alias', '')
+    splits = message_text.split(" == ")
+    if len(splits) < 2:
+        bot.reply_to(message, empty_alias_add_request)
+        return
+
+    key = (splits[0]).strip().lower()
+    value = (splits[1]).strip().lower()
+
+    result = aliases_manager.add_alias(key, value)
+
+    if result == 1:
+        bot.reply_to(message, "Alias було успіно додано")
+    if result == 0:
+        bot.reply_to(message, "Alias вже існує, тому зміна не відбулась")
+
+
+@bot.message_handler(commands=['show_aliases'])
+def show_all_aliases(message):
+    data = aliases_manager.get_all_aliases()
+    if len(data) == 0:
+        bot.reply_to(message, "Список alias наразі пустий")
+    res = ""
+    for key, value in data.items():
+        res += "• " + key + " == " + value + "\n"
+    bot.reply_to(message, res, parse_mode="HTML")
+
+
+@bot.message_handler(commands=['delete_alias'])
+def delete_alias(message):
+    message_text = (message.text.replace('/delete_alias', '')).strip()
+    if message_text == '':
+        bot.reply_to(message, empty_alias_delete_request)
+        return
+
+    res = aliases_manager.delete_alias(message_text)
+    if res == 1:
+        bot.reply_to(message, "Alias було видалено")
+    else:
+        bot.reply_to(message, "Alias з таким ім'ям не існує")
+
+
+"""
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
@@ -47,3 +110,8 @@ def webhook():
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    """
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.polling(none_stop=True, interval=0)
